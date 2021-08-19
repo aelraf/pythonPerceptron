1918fc681e94357228a591a4e072275342902589
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-08-19 11:42:16 +0200

    perceptron - początki menu

--- main.py
+++ main.py
@@ -7,7 +7,28 @@
 # później perceptrona
 # "liczby" do rozpoznawania są zapisywane 0/1, gdzie 1 <=> czarny, 0 <=> biały
 
+import pygame
+
+
+pygame.init()
+resolution = (1280, 720)
+window = pygame.display.set_mode(resolution)
+
+
+def main():
+    run = True
+    clock = 0
+
+    while run:
+        clock += pygame.time.Clock().tick(60)/1000
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
 
 if __name__ == '__main__':
-    pass
+    main()
 
