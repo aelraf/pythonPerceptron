b37de1cdb7e4fc83fb9fb2f6148d1a981a9d70c6
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-08-19 11:35:43 +0200

    commit inicjujący

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# RafKac
+# 2021_08_19
+#
+# program realizujący Perceptron do rozpoznawania liczb, na podstawie mojego kodu z C++
+# najpierw tworzymy GUI
+# później perceptrona
+# "liczby" do rozpoznawania są zapisywane 0/1, gdzie 1 <=> czarny, 0 <=> biały
+
+
+if __name__ == '__main__':
+    pass
+
