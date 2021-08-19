24fa68543d1280a45decfcfa1d07d9fca72332ae
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-08-19 14:54:26 +0200

    perceptron - klasa Przycisk()

--- main.py
+++ main.py
@@ -36,6 +36,34 @@ class superPixel:
     def draw(self, win, background):
         pygame.draw.rect(win, self.kolor, self.kwadrat)
 
+"""
+Zmienne klasowe:
+- współrzędne x i y
+- obraz wyświtlany normalnie i po najechaniu myszą
+- pole pod przyciskiem, przechwytujące akcje myszy
+
+metoda klikPrzycisk() zwraca True, jeśli klinęliśmy LPM
+metoda draw(win) kontroluje wyświetlany obraz (w zależności, czy najechaliśmy myszą, czy nie)
+"""
+class Przycisk:
+    def __init__(self, x, y, nazwaPliku):
+        self.x_cord = x
+        self.y_cord = y
+        self.obrazPrzycisku = pygame.image.load(f"{nazwaPliku}.png")
+        self.obrazKlikniety = pygame.image.load(f"{nazwaPliku}klik.png")
+        self.polePrzycisku = pygame.Rect(self.x_cord, self.y_cord, self.obrazPrzycisku.get_width(), self.obrazPrzycisku.get_height())
+
+    def klikPrzycisk(self):
+        if self.polePrzycisku.collidepoint(pygame.mouse.get_pos()):
+            if pygame.mouse.get_pressed()[0]:
+                return True
+
+    def draw(self, win):
+        if self.polePrzycisku.collidepoint(pygame.mouse.get_pos()):
+            win.blit(self.obrazKlikniety, (self.x_cord, self.y_cord))
+        else:
+            win.blit(self.obrazPrzycisku, (self.x_cord, self.y_cord))
+
 
 def main():
     run = True
