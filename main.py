68dc48ec1459d5df89201760a1516288572335ca
Rafał Kaczorkiewicz <rekin5-92@o2.pl>
2021-08-27 16:51:55 +0200

    klasa Przycisk jako oddzielny plik

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # 2021_08_19
 # 2021_08_23
 # 2021_08_25
+# 2021_08_26
+# 2021_08_27
 #
 # program realizujący Perceptron do rozpoznawania liczb, na podstawie mojego kodu z C++
 # najpierw tworzymy GUI
@@ -15,6 +17,7 @@ import SuperPixel
 import Przyklady
 import Wynik
 import time
+import Przycisk
 
 
 pygame.init()
@@ -22,49 +25,8 @@ resolution = (400, 300)
 window = pygame.display.set_mode(resolution)
 run = True
 listaSuperpixeli = []
+listaPerceptronow =[]
 wynik = Wynik.Wynik(200, 50, 50, 50)
-
-
-class Przycisk:
-    """
-    Zmienne klasowe:
-     - współrzędne x i y
-     - obraz wyświtlany normalnie i po najechaniu myszą
-     - pole pod przyciskiem, przechwytujące akcje myszy
-
-    metoda klikPrzycisk() zwraca True, jeśli klinęliśmy LPM
-    metoda draw(win) kontroluje wyświetlany obraz (w zależności, czy najechaliśmy myszą, czy nie)
-
-    zmienna "nazwa" kontroluje akcję po wciśnięciu przycisku
-    """
-    def __init__(self, x, y, nazwaPliku):
-        self.x_cord = x
-        self.y_cord = y
-        self.obrazPrzycisku = pygame.image.load(f"{nazwaPliku}.png")
-        self.obrazKlikniety = pygame.image.load(f"{nazwaPliku}klik.png")
-        self.polePrzycisku = pygame.Rect(self.x_cord, self.y_cord, self.obrazPrzycisku.get_width(), self.obrazPrzycisku.get_height())
-        self.nazwa = nazwaPliku[8:]
-        self.status = False
-
-    def klikPrzycisk(self):
-        if self.polePrzycisku.collidepoint(pygame.mouse.get_pos()):
-            if pygame.mouse.get_pressed()[0]:
-                self.status = True
-                return True
-        self.status = False
-        return False
-
-    def draw(self, win):
-        if self.polePrzycisku.collidepoint(pygame.mouse.get_pos()):
-            win.blit(self.obrazKlikniety, (self.x_cord, self.y_cord))
-        else:
-            win.blit(self.obrazPrzycisku, (self.x_cord, self.y_cord))
-
-    def setStatus(self, status):
-        self.status = status
-
-    def getStatus(self):
-        return self.status
 
 
 def start():
@@ -110,13 +72,13 @@ def main():
     positionX = 0
     positionY = 0
     tabP = []
-    przycisk_nauka = Przycisk(300, 40, "buttons/nauka")
+    przycisk_nauka = Przycisk.Przycisk(300, 40, "buttons/nauka")
     tabP.append(przycisk_nauka)
-    przycisk_start = Przycisk(300, 80, "buttons/start")
+    przycisk_start = Przycisk.Przycisk(300, 80, "buttons/start")
     tabP.append(przycisk_start)
-    przycisk_stop = Przycisk(300, 120, "buttons/stop")
+    przycisk_stop = Przycisk.Przycisk(300, 120, "buttons/stop")
     tabP.append(przycisk_stop)
-    przycisk_koniec = Przycisk(300, 160, "buttons/koniec")
+    przycisk_koniec = Przycisk.Przycisk(300, 160, "buttons/koniec")
     tabP.append(przycisk_koniec)
 
     x = 10
@@ -133,7 +95,7 @@ def main():
     przyklady = Przyklady.Przyklady()
     przykladyTestowe = Przyklady.Przyklady()
     przyklady.dodajPrzyklady()
-    przykladyTestowe
+    przykladyTestowe.dodajPrzykladyTestowe()
 #    for p in przyklady.listaPrzykladow:
 #        rysuj(p)
 
